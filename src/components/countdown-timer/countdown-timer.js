b6428b6a3598523a3f745be36e6414b2fdad596a
Farzad Fahiminia <ff222cb@student.lnu.se>
2021-01-10 14:02:24 +0100

    Add saved files to commit

--- src/components/countdown-timer/countdown-timer.js
+++ src/components/countdown-timer/countdown-timer.js
@@ -15,7 +15,6 @@ template.innerHTML = `
       max-width: 600px;
       font-size: 1.2em;
       padding: 3px 15px 15px 15px;
-      /* margin: 1em; */
       background-color: #f7f7f7;
     }
   </style>
