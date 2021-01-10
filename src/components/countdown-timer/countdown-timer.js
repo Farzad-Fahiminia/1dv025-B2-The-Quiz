61b6cd7f50048405472e5e59aff98391e3436d56
Farzad Fahiminia <ff222cb@student.lnu.se>
2021-01-10 13:56:29 +0100

    Add countown section to index.html

--- src/components/countdown-timer/countdown-timer.js
+++ src/components/countdown-timer/countdown-timer.js
@@ -4,3 +4,42 @@
  * @author Farzad Fahiminia <ff222cb@student.lnu.se>
  * @version 1.0.0
  */
+
+ /**
+ * Define template.
+ */
+const template = document.createElement('template')
+template.innerHTML = `
+  <style>
+    .countdown-timer {
+      max-width: 600px;
+      font-size: 1.2em;
+      padding: 3px 15px 15px 15px;
+      /* margin: 1em; */
+      background-color: #f7f7f7;
+    }
+  </style>
+  <div class="countdown-timer">
+   <h2>Countdown timer</h2>
+   <p>Time left...</p>
+  </div>
+`
+
+/**
+ * Define custom element.
+ */
+customElements.define('countdown-timer',
+  class extends HTMLElement {
+    /**
+     * Creates an instance of the current type.
+     */
+    constructor () {
+      super()
+
+      // Attach a shadow DOM tree to this element and
+      // append the template to the shadow root.
+      this.attachShadow({ mode: 'open' })
+        .appendChild(template.content.cloneNode(true))
+    }
+  }
+)
--- src/components/high-score/high-score.js
+++ src/components/high-score/high-score.js
@@ -4,3 +4,42 @@
  * @author Farzad Fahiminia <ff222cb@student.lnu.se>
  * @version 1.0.0
  */
+
+ /**
+ * Define template.
+ */
+const template = document.createElement('template')
+template.innerHTML = `
+  <style>
+    .high-score {
+      max-width: 600px;
+      font-size: 1.2em;
+      padding: 3px 15px 15px 15px;
+      /* margin: 1em; */
+      background-color: #f7f7f7;
+    }
+  </style>
+  <div class="high-score">
+   <h2>High Score</h2>
+   <p>Are you on the top 5 list?</p>
+  </div>
+`
+
+/**
+ * Define custom element.
+ */
+customElements.define('high-score',
+  class extends HTMLElement {
+    /**
+     * Creates an instance of the current type.
+     */
+    constructor () {
+      super()
+
+      // Attach a shadow DOM tree to this element and
+      // append the template to the shadow root.
+      this.attachShadow({ mode: 'open' })
+        .appendChild(template.content.cloneNode(true))
+    }
+  }
+)
